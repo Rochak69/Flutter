dc2463198edcea7425be488f5d27555332065a23
Rochak Shrestha <rochakshrestah666@gmail.com>
2022-06-23 16:39:00 +0545

    Feat: nultiple dart files and passing functions on constructors

--- initial_project/lib/main.dart
+++ initial_project/lib/main.dart
@@ -1,52 +1,50 @@
 import 'package:flutter/material.dart';
+import './questions.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-
   @override
-  State<StatefulWidget> createState(){
+  State<StatefulWidget> createState() {
     return MyAppState();
   }
 }
-class MyAppState extends State<MyApp>{
-  var questionIndex= 0;
 
-  void answerQuestion(){
-    setState((){
-      questionIndex= questionIndex +1;
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
     });
 
     print("answer 1");
-    print(questionIndex);
+
+
+
   }
-  void answerQuestion1(){
-    print("answer 2");
-  }
+
 
   @override
   Widget build(BuildContext context) {
-    var questions = ["What\'s your favrite color?", "What\'s your favorite animal"];
+    var questions = [
+      "What\'s your favorite color?",
+      "What\'s your favorite animal"
+    ];
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(title: Text("Initial App")),
+      appBar: AppBar(title: Text("Quiz App")),
       body: Column(
         children: [
-          Text(questions.elementAt(questionIndex)),
-          RaisedButton(
-            child: Text('Answer 1'),
-            onPressed: answerQuestion,
+          Questions(
+              questions.elementAt(questionIndex)
           ),
-          RaisedButton(
-            child: Text("Answer2"),
-            onPressed: answerQuestion1,
-          ),
-          RaisedButton(
-            child: Text('Answer 1'),
-            onPressed: ()=> print("Answer 3"),
-          ),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion),
         ],
       ),
     ));
