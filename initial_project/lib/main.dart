70f0d6775b84cc326dc602a377c77cc11a5081c8
Rochak Shrestha <rochakshrestah666@gmail.com>
2022-06-23 12:42:58 +0545

    Feat: Stateful widjet implementation

--- initial_project/lib/main.dart
+++ initial_project/lib/main.dart
@@ -4,29 +4,43 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState(){
+    return MyAppState();
+  }
+}
+class MyAppState extends State<MyApp>{
+  var questionIndex= 0;
+
   void answerQuestion(){
+    setState((){
+      questionIndex= questionIndex +1;
+    });
+
     print("answer 1");
+    print(questionIndex);
   }
   void answerQuestion1(){
     print("answer 2");
   }
- 
+
   @override
   Widget build(BuildContext context) {
-    var questions = ["wwww", "wwww"];
+    var questions = ["What\'s your favrite color?", "What\'s your favorite animal"];
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(title: Text("Initial App")),
       body: Column(
         children: [
-          Text("The questions"),
+          Text(questions.elementAt(questionIndex)),
           RaisedButton(
             child: Text('Answer 1'),
             onPressed: answerQuestion,
           ),
           RaisedButton(
-            child: Text('Answer 1'),
+            child: Text("Answer2"),
             onPressed: answerQuestion1,
           ),
           RaisedButton(
